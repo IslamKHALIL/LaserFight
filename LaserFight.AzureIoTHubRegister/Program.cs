883158ebd5d74d7ca3be8f0eac2fcae9bc2a0e9d
Islam KHALIL <islam.ossama@hotmail.fr>
2015-10-30 16:09:56 +0200

    Adding Azure IoT Hub register console application

---
+++ LaserFight.AzureIoTHubRegister/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace LaserFight.AzureIoTHubRegister
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+        static string connectionString = "{iothub connection string}";
+
+        static void Main(string[] args)
+        {
+            AddDeviceAsync("<the device's id that will be given to the player>");
+        }
+
+        private async static Task AddDeviceAsync(string deviceId)
+        {
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+            Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
+        }
+    }
+}
